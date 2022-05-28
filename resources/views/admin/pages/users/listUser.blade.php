fa8a86185b6c5163ca92e8b94b9078aac0ad60e1
duongvanhuy <duongvanhuy137@gmail.com>
2022-05-28 20:47:14 +0700

    feat: add validate form create user

--- resources/views/admin/pages/users/listUser.blade.php
+++ resources/views/admin/pages/users/listUser.blade.php
@@ -3,6 +3,7 @@
     <link rel="stylesheet" href="{{asset('AdminPTH/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')}}">
     <link rel="stylesheet" href="{{asset('AdminPTH/plugins/datatables-responsive/css/responsive.bootstrap4.min.css')}}">
     <link rel="stylesheet" href="{{asset('AdminPTH/plugins/datatables-buttons/css/buttons.bootstrap4.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/validate.css')}}">
     <style>
         div#example1_length {
     display: contents;
@@ -37,8 +38,8 @@ div.dataTables_wrapper div.dataTables_filter label {
     </button>-->
     <div id="PlaceHolderHere"></div>
     <div id="ShowModal"></div>
-    <button type="button" class="btn btn-primary" data-toggle="ajax-modal"
-            data-target="#modal-create" data-url="@Url.Action("Create")">
+    <button type="button" class="btn btn-primary" data-toggle="modal"
+            data-target="#modal-create">
         Thêm người dùng
     </button>
 </div>
@@ -69,6 +70,72 @@ div.dataTables_wrapper div.dataTables_filter label {
     </div>
 </div>
 </div>
+
+
+<!-- Model create-->
+<div class="modal fade" id="modal-create" data-backdrop="static">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Thêm người dùng</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form asp-action="Create" id="create-user">
+                    <div class=" card-body">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="name">Họ & tên</label>
+                                    <input id="name" type="text" class="form-control" name="name" placeholder="Enter name">
+                                    
+                                    <span class="form-message"></span>
+                                </div>
+                                <div class="form-group">
+                                    <label for="email">Địa chỉ Email</label>
+                                    <input id="email" type="email" class="form-control" name="email" placeholder="Enter email">
+                                    <span class="form-message"></span>
+                                    <span id="message" class="text-danger"></span>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="phone">Số điện thoại</label>
+                                    <input id="phone" type="text" class="form-control" name="phone" placeholder="Enter Phone">                                    
+                                    <span class="form-message"></span>
+
+                                </div>
+                                <div class="form-group">
+                                    <label for="birthday">Ngày sinh</label>
+                                    <input id="birthday"  type="date" value="2022-05-25" name="birthday" class="form-control" placeholder="Enter Birthday">                                 
+                                    <span class="form-message"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="Address">Địa chỉ </label>
+                            <input for="Address" type="text" class="form-control" name="address" placeholder="Enter Address">
+                            
+                            <span class="form-message"></span>
+                        </div>
+                          <div class="modal-footer justify-content-between">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Thoát</button>
+                            <button type="submit" class="btn btn-primary" data-save="modal">Lưu</button>
+                        </div>
+                    </div>
+                </form>
+              
+            </div>
+
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
+<!-- /Model create-->
+
 @endsection
     <!-- /.row -->
 
@@ -87,18 +154,9 @@ div.dataTables_wrapper div.dataTables_filter label {
     <script src="{{asset('AdminPTH/plugins/datatables-buttons/js/buttons.print.min.js')}}"></script>
     <script src="{{asset('AdminPTH/plugins/datatables-buttons/js/buttons.colVis.min.js')}}"></script>
     <script>
-    // $(function() {
-    //     $("#example1").DataTable({  
-    //         "responsive": true,
-    //         "lengthChange": false,
-    //         "autoWidth": false,
-    //         "buttons": ["copy", "csv", "excel", "pdf", "print"]
-    //     }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
-    // });
+
+       
     </script>
-
-
-
 @*message*@
 <!-- SweetAlert2 -->
 <script src="{{asset('AdminPTH/plugins/sweetalert2/sweetalert2.min.js')}}"></script>
@@ -109,7 +167,7 @@ div.dataTables_wrapper div.dataTables_filter label {
 <script>
 
     $(document).ready(function(){
-       
+    //    datatable
         $("#example1").DataTable({
             processing: true, 
             serverSide: true, 
@@ -119,7 +177,7 @@ div.dataTables_wrapper div.dataTables_filter label {
             dom: 'Blfrtip',
             buttons: [
             'copy', 'csv', 'excel', 'pdf', 'print'
-        ],
+            ],
             ajax: {
                 url:  "{{asset('admin/users/getList')}}",
                 // type: 'get',
@@ -151,51 +209,129 @@ div.dataTables_wrapper div.dataTables_filter label {
                 }
             },
             ],
-        })
+        });
+    // send data and validate
+        var PlaceHolderElement = $("#PlaceHolderHere");
+
+      // xử lý -  gửi data sau khi thông qua được validate
+      $.validator.setDefaults({
+       submitHandler: function () {
+            PlaceHolderElement.on('click', '[data-save="modal"]', function (event) {
+              
+            // cấu hình message
+                var Toast = Swal.mixin({
+                    toast: true,
+                    position: 'top-end',
+                    showConfirmButton: false,
+                    timer: 3000
+                });
+                console.log("save")
+                // form
+                var form = $(this).parents('.modal').find('form');
+                var actionUrl = form.attr('action');
+                console.log(form, actionUrl)
+                var sendData = form.serialize();
+           
+                $.post(actionUrl, sendData).done(function (data) {
+                    Toast.fire({
+                        icon: 'success',
+                        title: "Thông tin được lưu lại thành công"
+                    })
+                    PlaceHolderElement.find('.modal').modal('hide')
+                     $('#example1').DataTable().ajax.reload();
+                     return;
+                })
+               document.getElementById('message').innerHTML ="email đã tồn tại"
+            });
+        }
+      });
+      $('#create-user').validate({
+        rules: {
+          name: {
+            required: true,
+          },
+          email: {
+            required: true,
+            email: true,
+          },
+          phone: {
+            required: true,
+            phoneUK: true,
+           
+          },
+          address: {
+            required: true,
+          },
+        },
+        messages: {
+          email: {
+            required: "Vui lòng nhập địa chỉ email",
+            email: "Trường này là email"
+          },
+          name: {
+            required: "Vui lòng nhập Họ $ tên",
+          },
+          phone: {
+            required: "Vui lòng nhập số điện thoại",
+            phoneUS: "Trường này là số điện thoại"
+          },
+          address: {
+            required: "Vui lòng nhập địa chỉ",
+          },
+        },
+        errorElement: 'span',
+        errorPlacement: function (error, element) {
+          error.addClass('invalid-feedback');
+          element.closest('.form-group').append(error);
+        },
+        highlight: function (element, errorClass, validClass) {
+          $(element).addClass('is-invalid');
+        },
+        unhighlight: function (element, errorClass, validClass) {
+          $(element).removeClass('is-invalid');
+        }
+      });
     })
     
     // hiện thị modal edit
-    var ShowModal = $("#ShowModal");
-    function EditUser(url){
-        var decodeUrl = decodeURIComponent(url);
-        $.get(decodeUrl).done(function (data) {
-            ShowModal.html(data);
-            ShowModal.find('.modal').modal('show');
-        })
-    }
-     // lưu thông tin trong form edit khi button save
-    ShowModal.on('click', '[data-save="modal"]', function (event) {
-        event.preventDefault();
+    // var ShowModal = $("#ShowModal");
+    // function EditUser(url){
+    //     var decodeUrl = decodeURIComponent(url);
+    //     $.get(decodeUrl).done(function (data) {
+    //         ShowModal.html(data);
+    //         ShowModal.find('.modal').modal('show');
+    //     })
+    // }
+    //  // lưu thông tin trong form edit khi button save
+    // ShowModal.on('click', '[data-save="modal"]', function (event) {
+    //     event.preventDefault();
 
-        var Toast = Swal.mixin({
-            toast: true,
-            position: 'top-end',
-            showConfirmButton: false,
-            timer: 3000
-        });
+    //     var Toast = Swal.mixin({
+    //         toast: true,
+    //         position: 'top-end',
+    //         showConfirmButton: false,
+    //         timer: 3000
+    //     });
 
-        var form = $(this).parents('.modal').find('form');
-        var actionUrl = form.attr('action');
-        var sendData = form.serialize();
+    //     var form = $(this).parents('.modal').find('form');
+    //     var actionUrl = form.attr('action');
+    //     var sendData = form.serialize();
 
-        $.post(actionUrl, sendData).done(function (data) {
+    //     $.post(actionUrl, sendData).done(function (data) {
            
-            Toast.fire({
-                icon: 'success',
-                title: "Thông tin được lưu lại thành công"
-            })
-            ShowModal.find('.modal').modal('hide')
-          $('#example1').DataTable().ajax.reload();
-        })
-    })
-   
-    //var inputs = document.getElementsByClassName('form-control form-control-sm').addEventListener("keyup", ()=>{
-    //    console.log('aaaaa')
-    //});
-    document.addEventListener("keyup", ()=>{
-        console.log("aa")
-    })
+    //         Toast.fire({
+    //             icon: 'success',
+    //             title: "Thông tin được lưu lại thành công"
+    //         })
+    //         ShowModal.find('.modal').modal('hide')
+    //       $('#example1').DataTable().ajax.reload();
+    //     })
+    // })
+    // document.addEventListener("keyup", ()=>{
+    //     console.log("aa")
+    // })
+
 </script>
 
- 
+
 @endsection
